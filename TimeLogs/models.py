e7b87a9930a7fdbfd1c5b6e13921c3d6d4579b1c
ash-R2D2 <avinavashish008@gmail.com>
2023-03-10 20:50:39 +0530

    created data models.

--- TimeLogs/models.py
+++ TimeLogs/models.py
@@ -1,4 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class TimeLogs(models.Model):
+    team = models.CharField(max_length=250)
+    username = models.CharField(max_length=250)
+    job_number = models.CharField(max_length=250)
+    booking_codes = models.CharField(max_length=250)
+    booking_date = models.DateField(auto_now=False)
+    time_tracked = models.DecimalField(max_digits=5, decimal_places=1)
+    task_estimate = models.DecimalField(max_digits=5, decimal_places=1)
+
 
