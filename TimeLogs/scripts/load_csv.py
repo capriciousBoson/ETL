eb7252debe72b16e0c137a2d8d1996c635e1e25f
ash-R2D2 <avinavashish008@gmail.com>
2023-03-11 02:37:21 +0530

    created script for loading csv to django sql database.

---
+++ TimeLogs/scripts/load_csv.py
@@ -0,0 +1,41 @@
+from TimeLogs.models import TimeLogs
+from dateutil.parser import parse
+import csv
+
+def format_date(date_str):
+    date_object = parse(date_str, tzinfos={"BST": 6 * 3600})
+    return date_object.strftime("%d-%m-%Y")
+
+
+def format_time(time_str):
+    if time_str == "":
+        time_str = 0.0
+    else:
+        time_str = round(int(time_str) / 3600000, 1)
+    return time_str
+
+
+def run():
+    with open('TimeLogs/data/TIME-LOGS-CHALLANGE.xlsx') as file:
+        reader = csv.reader(file)
+
+        # to skip the header
+        next(reader)
+
+        for row in reader:
+            team_ = row[41]
+            username_ = row[1]
+            job_number_ = row[14]
+            booking_codes_ = row[39]
+            booking_date_ = format_date(row[8])
+            time_tracked_ = format_time(row[9])
+            task_estimate_ = format_time(row[23])
+
+            timelog = TimeLogs(team=team_,
+                               username=username_,
+                               job_number=job_number_,
+                               booking_codes=booking_codes_,
+                               booking_date=booking_date_,
+                               time_tracked=time_tracked_,
+                               task_estimate=task_estimate_)
+            timelog.save()
