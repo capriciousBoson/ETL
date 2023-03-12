88511f54385026af412596f0dd02718f28acb267
ash-R2D2 <avinavashish008@gmail.com>
2023-03-12 09:57:54 +0530

    Pagination done!

--- app/src/components/timelogs_table.vue
+++ app/src/components/timelogs_table.vue
@@ -1,7 +1,9 @@
 <template>
-    <table class="table">
+  <div class="container">
+    <table class="table table-striped">
       <thead class="thead-dark">
         <tr>
+          <th>No.</th>
           <th>Team</th>
           <th>Username</th>
           <th>Job Number</th>
@@ -12,7 +14,8 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-for="item in items" :key="item.id">
+        <tr v-for="(item, index) in slicedItems" :key="item.id">
+          <td>{{ (currentPage - 1) * itemsPerPage + index + 1 }}</td>
           <td>{{ item.team }}</td>
           <td>{{ item.username }}</td>
           <td>{{ item.job_number }}</td>
@@ -23,6 +26,12 @@
         </tr>
       </tbody>
     </table>
+  </div>
+    <div  class="pagination centered">
+      <button class="btn btn-success" :disabled="currentPage === 1" @click="previousPage">Previous</button>
+      <span style="margin-left: 50px;">Page {{ currentPage }} of {{ pageCount }}</span>
+      <button class="btn btn-success" style="margin-left: 50px;" :disabled="currentPage === pageCount" @click="nextPage">Next</button>
+    </div>
   </template>
   
   <script>
@@ -31,10 +40,39 @@
   export default {
     data() {
       return {
+        currentPage:1, //stores the current page number
+        itemsPerPage:10, 
         items: []
       }
     },
+    computed:{
+      //a computed property that returns the 
+      //sliced array of items based on the current page 
+      // and items per page
+      slicedItems(){
+        const start = (this.currentPage - 1)* this.itemsPerPage
+        const end = start + this.itemsPerPage
+        return this.items.slice(start, end)
+      },
+      pageCount() {
+        return Math.ceil(this.items.length / this.itemsPerPage)
+      }
+    },
+    methods:{
+  previousPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--
+    }
+  },
+  nextPage() {
+    if (this.currentPage < this.pageCount) {
+      this.currentPage++
+    }
+  }
+      
+    },
     created() {
+      
       axios.get('http://localhost:8000/timelogs/api/')
         .then(response => {
           this.items = response.data;
@@ -42,7 +80,19 @@
         .catch(error => {
           console.log(error);
         });
-    }
-  }
+        window.addEventListener('keydown', this.handleKeyDown)
+    },
+    beforeUnmount() {
+    window.removeEventListener("keydown", this.handleKeyDown);
+  },
+    
+
+  };
   </script>
-  
+
+  <style scoped>
+  .centered {
+  display: flex;
+  justify-content: center;
+}
+</style>
